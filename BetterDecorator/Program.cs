3da9ffa111574db06ed7f3aaebfc9269b0e9181e
LuanVelho <146845889+LuanVelho@users.noreply.github.com>
2024-03-14 19:44:12 -0400

    BetterDecorator defaltu funcionando

---
+++ BetterDecorator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BetterDecorator
+{
+    class Program
+    {
+        private static string Decor(string s, char dec)
+        {
+            return $"{dec}{dec}{dec} {s} {dec}{dec}{dec}";
+        }
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(Decor("Good Morning!", '*'));
+            Console.WriteLine(Decor("Good Morning!", '.'));
+        }
+    }
+}
