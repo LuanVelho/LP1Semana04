4880a1f03ef9299f4d30aa08e2a53a0b6dc8c717
LuanVelho <146845889+LuanVelho@users.noreply.github.com>
2024-03-14 20:04:29 -0400

    Exercicio 2 concluido, comentários XML adicionados

--- BetterDecorator/Program.cs
+++ BetterDecorator/Program.cs
@@ -4,10 +4,21 @@ namespace BetterDecorator
 {
     class Program
     {
+        /// <summary>
+        /// Adiciona um charactere no começo e no fim da string o número de vezes indicado
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="dec"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
         private static string Decor(string s, char dec, int n)
         {
             return $"{"dec"*n} {s} {"dec"*n}";
         }
+        /// <summary>
+        /// Entrega os parametros necessários para o Decor funcionar
+        /// </summary>
+        /// <param name="args"></param>
         private static void Main(string[] args)
         {
             Console.WriteLine(Decor("Good Morning!", '*', 4));
