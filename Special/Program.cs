7b182843a4288bbccb70d152f544153f2d78a8c8
LuanVelho <146845889+LuanVelho@users.noreply.github.com>
2024-03-14 21:38:40 -0400

    Exercício 4

---
+++ Special/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Special
+{
+    class Program
+    {
+        private static int GetSpecial(int n)
+        {
+            int [] Sequencia = [0,1,1,3,5,11,21,43,85,171,341];
+            return Sequencia[n];
+        }
+        static void Main(string[] args)
+        {
+         Console.Write("Insert n-th number of the special sequence: ");
+         resposta = Console.Read();
+         GetSpecial(resposta);   
+        }
+    }
+}
